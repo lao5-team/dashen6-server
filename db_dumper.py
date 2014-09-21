c97467322459e6e8b0d5b5cab457b6f164331a62
yafei <yafei@yahoo-inc.com>
2014-09-21 17:25:55 +0000

    improve db_dumper

--- db_dumper.py
+++ db_dumper.py
@@ -3,6 +3,8 @@
 #
 # author: yafei
 # dump mongodb collections
+import json
+import sys
 from db_op import DBOp
 
 if __name__ == '__main__':
@@ -15,3 +17,11 @@ if __name__ == '__main__':
     print 'dumping collection "activity"'
     for a in db.activity.find():
         print a
+
+        # if a has 'data' field, try to parse it
+        data = a.get('data')
+        if data:
+            try:
+                json.loads(data)
+            except ValueError:
+                print >> sys.stderr, '''couldn't decode data'''
