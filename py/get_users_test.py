b9533104f7aa68ee04f406d1487599d7ae0a6f2f
yihao <yhchinabest@gmail.com>
2014-10-25 10:29:14 +0800

    modify test

--- py/get_users_test.py
+++ py/get_users_test.py
@@ -12,3 +12,8 @@ activity = db['user']
 
 for item in activity.find():
     print item
+
+post = activity.find_and_modify(
+    query={'_id': ObjectId('54492c4e42a953444a74450f')},
+    update={'$set': {'status': STATUS_DELETED}},
+    fields=['_id'])
