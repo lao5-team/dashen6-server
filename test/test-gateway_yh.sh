43a4a4479d783eb832fff751a4570e8a02e4a996
yihao <yhchinabest@gmail.com>
2014-11-01 11:36:33 +0800

    modify test

--- test/test-gateway_yh.sh
+++ test/test-gateway_yh.sh
@@ -32,7 +32,7 @@ user_data=$(curl -v "${url}/db?action=get_user&username=yihao" -b cookie -d dumm
 r=$(cat stderr | grep " 200 OK")
 CheckNotEmpty "$user_data" "get_user"
 
-data=$(curl -v "${url}/db?action=add_user_activity&field=doing&activity_id=activity0" -b cookie -d dummy 2>stderr | python ./json_helper.py data)
+data=$(curl -v "${url}/db?action=add_user_activity&field=doing&activity_id=activity0" -b cookie -d dummy 2>stderr)
 r=$(cat stderr | grep " 200 OK")
 CheckNotEmpty "$r" "db add_user_activity"
 
