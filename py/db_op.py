80513f07af6784cc1ed24e95c0be6757e2e79547
yihao <yhchinabest@gmail.com>
2014-11-01 14:43:56 +0800

    modify test

--- py/db_op.py
+++ py/db_op.py
@@ -246,5 +246,5 @@ class DBOp:
         table = self.get_safe_table(db_user_activity_table)
         post = table.find_one({'_id': user_id}, fields=fields)
         if post is None:
-            raise Exception('''Couldn't load user_id=%s, it doesn't exist or deleted.''' % username)
+            raise Exception('''Couldn't load user_id=%s, it doesn't exist or deleted.''' % user_id)
         return post
