f1ffb2fcb893b0d831b20683a7cb3f12662d2dde
yh <yhchinabest@gmail.com>
2014-11-02 22:51:03 +0800

    delete some documents
    Signed-off-by: yh <yhchinabest@gmail.com>

--- py/get_users_test.py
+++ py/get_users_test.py
@@ -10,10 +10,11 @@ from bson.objectid import ObjectId
 STATUS_DELETED = 'deleted'
 con = pymongo.Connection('localhost', 27017)
 db = con['db']
-activity = db['user']
+user = db['user']
 user_activity = db['user_activity']
+user.remove({"username":"kimi1"})
 
-for item in activity.find():
+for item in user.find():
     print item
 
 for item in user_activity.find():
