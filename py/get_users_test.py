74a3085506f2f4c226c06ae2d9e7c0fa34971e45
yh <yhchinabest@gmail.com>
2014-11-17 18:18:05 +0800

    add some utils
    Signed-off-by: yh <yhchinabest@gmail.com>

--- py/get_users_test.py
+++ py/get_users_test.py
@@ -11,20 +11,33 @@ STATUS_DELETED = 'deleted'
 con = pymongo.Connection('localhost', 27017)
 db = con['db']
 user = db['user']
+activity = db['activity']
 user_activity = db['user_activity']
 user_message = db['user_message']
 message = db['message']
-#user_message.remove()
-#message.remove()
-
+'''
+user_message.remove()
+message.remove()
+activity.remove()
+user_activity.remove();
+'''
+print "user table"
 for item in user.find():
     print item
 
+print "activity_table"
+for item in activity.find():
+    print item
+
+
+print "user_activity table"
 for item in user_activity.find():
     print item
 
+print "user_message table"
 for item in user_message.find():
     print item
 
+print "message table"
 for item in message.find():
     print item
