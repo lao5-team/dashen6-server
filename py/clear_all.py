e036d08f807a599bae78fcece5c86b9116c3f9d4
yihao <yhchinabest@gmail.com>
2014-12-02 12:19:52 +0800

    add clear comment

--- py/clear_all.py
+++ py/clear_all.py
@@ -17,8 +17,10 @@ user_activity = db['user_activity']
 user_message = db['user_message']
 message = db['message']
 picture_info = db['picture_info']
+comment = db['comment']
 user_message.remove()
 message.remove()
-#activity.remove()
-activity.remove({'_id':ObjectId('546c95e9e138236804e093f5')})
-user_activity.remove();
+activity.remove()
+#activity.remove({'_id':ObjectId('546c95e9e138236804e093f5')})
+user_activity.remove()
+comment.remove()
